aaffacef60443750a7e2e8fd89a51a8dbd2aa120
Yokasah <tomasdiasmorais@gmail.com>
2021-08-18 21:08:45 +0100

    inicial commit

---
+++ connection.php
@@ -0,0 +1,18 @@
+<!--Made By Tomás Morais-->
+
+
+<?php
+$servername = "localhost";
+$username = "example";
+$password = "1234";
+
+//The username and password in this case are examples,you have to put the user from your own PHPMyAdmin and use it 
+//in the rest of the project
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=db_alphasphere",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo "Connection Failed: " . $e->getMessage();
+}
+?>
